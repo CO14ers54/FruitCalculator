a8014e53f6e9120a826fbbb24d8a2386e9067621
CO14ers54 <CO14ers54@gmail.com>
2020-12-04 16:35:17 -0700

    Add project files.

---
+++ FruitCalculator/Program.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FruitCalculator
+{
+   class Program
+   {
+      public enum TJCApplications
+      {
+         FruitCalculator
+      }
+
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Welcome to the Fruit Calculator!  This app calculates the cost of a fruit basket given fruit type, price, quantity and any promotions.");
+         Console.WriteLine();
+
+         try
+         {
+            // Get the Fruit, Promotions and Basket from a file so that if we want to change these inputs, we don't need to recompile
+            Dictionary<string, double> fruit = GetFruitAndPrices();
+            Dictionary<string, double> promotions = GetPromotions();
+            Dictionary<string, int> basket = GetBasket();
+
+            // if any of these lists are null, there is definitly a problem, so abort and give a message to the user.
+            if (basket == null)
+            {
+               Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator,"There was a problem reading in the fruit in the basket.  Canceling order...",Logger.MessageType.Error);
+            }
+            else if (fruit == null)
+            {
+               Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "There was a problem reading in the available fruit for sale.  Canceling order...", Logger.MessageType.Error);
+            }
+            else if (promotions == null)
+            {
+               Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "There was a problem reading in the promotions.  Canceling order...", Logger.MessageType.Error);
+            }
+            else
+            {
+               double totalPrice = ProcessFruitBasket(fruit, promotions, basket);
+
+               System.Text.StringBuilder sb = new System.Text.StringBuilder();
+               foreach (string key in basket.Keys)
+               {
+                  sb.Append(basket[key] + " " + key + ", ");
+               }
+
+               Console.WriteLine();
+               Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "This basket includes the following fruit: " + sb.ToString().Remove(sb.ToString().Length - 1).TrimEnd(), Logger.MessageType.Info);
+               Console.WriteLine();
+               Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "The total price for the basket is: $" + totalPrice.ToString("F"), Logger.MessageType.Mandatory);
+
+               // ALL IS GOOD IN THE JUNGLE!  THE LION SLEEPS TONIGHT!
+            }
+         }
+         catch (Exception ex)
+         {
+            Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, ex.Message, Logger.MessageType.Error);
+         }
+      }
+
+      /// <summary>
+      /// This method will check the inputs and if all is good, it will attempt to calculate the total price and return that result
+      /// </summary>
+      /// <param name="fruit"></param>
+      /// <param name="promotions"></param>
+      /// <param name="basket"></param>
+      /// <returns>double totalPrice</returns>
+      private static double ProcessFruitBasket(Dictionary<string, double> fruit, Dictionary<string, double> promotions, Dictionary<string, int> basket )
+      {
+         double totalPrice = 0.00;
+
+         try
+         {
+            // Check the inputs to make sure they are valid before proceding.  Log a message if something is amiss!
+            if ( basket.Count > 0 )
+            {
+               if ( fruit.Count < 1)
+               {
+                  Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "There was no fruit defined.", Logger.MessageType.Error);
+               }
+               else
+               {
+                  if (promotions.Count < 1)
+                  {
+                     Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "There are no promotions for this sale.", Logger.MessageType.Info);
+                  }
+                  // We are good to go ahead and attempt to calculate the total price!
+                  totalPrice = CalculateTotalPrice(fruit, promotions, basket);
+               }
+            }
+            else
+            {
+               Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "There was no fruit in the basket list.", Logger.MessageType.Error);
+            }
+         }
+         catch(Exception e)
+         {
+            Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, e.Message, Logger.MessageType.Error);
+         }
+
+         return totalPrice;
+      }
+
+      /// <summary>
+      /// This is the method that will actually calculate the price of the basket of fruit.  The parameters have already been checked for validity by the calling method,
+      /// but we will need to check the validity of the content.
+      /// </summary>
+      /// <param name="fruits"></param>
+      /// <param name="promotions"></param>
+      /// <param name="basket"></param>
+      /// <returns>The total price of the basket based on the price, quantity and deduction if there is a promotion</returns>
+      private static double CalculateTotalPrice(Dictionary<string, double> fruit, Dictionary<string, double> promotions, Dictionary<string, int> basket)
+      {
+         double totalPrice = 0.00;
+
+         try
+         {
+
+            // For efficency, start with the basket.  If only 3 fruit are required, don't need to go over up to 2 million different fruits with prices!
+            foreach( var item in basket.Keys)
+            {
+               int quantity = basket[item];
+               string fruitName = item;
+               double discount = -1.0;
+               double promotion = -1.0;
+               bool hasDiscount = false;
+
+               // ASSUMPTION:  THIS IS NOT STATED IN THE PROBLEM, BUT I AM ASSUMING THAT THE PROMOTION VALUE IS A PERCENT TAKEN OFF THE PRICE.  For example, a value of .25 means that 25% is taken
+               // off of the price.  Likewise, .75 means 75% taken off the price.  Therefore, the value must be 0 <= promotion <= 1 where a promotion of 1 means the item is free.
+               // A null promotion is valid here, so only worry about promotions if the promotion value is valid
+               //if (promotions.Count > 0)
+               //{
+               try
+               {
+                  // This will throw an exception if we try to reference a key that doesn't exist, so we must handle that case here.
+                  try
+                  {
+                     promotion = promotions[fruitName];
+
+                     if ((promotion < 0.00) || (promotion > 1.00))
+                     {
+                        Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "The promotion for " + fruitName + " was not between 0 and 1.  Canceling order...", Logger.MessageType.Error);
+                        totalPrice = 0.00;
+                        break;
+                     }
+                     else
+                     {
+                        hasDiscount = true;
+                        discount = 1 - promotion;
+                        Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "There is a promotion for " + fruitName + " of " + (discount * 100).ToString("F") + "% off.", Logger.MessageType.Info);
+                     }
+                  }
+                  catch(KeyNotFoundException ex)
+                  {
+                     Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "No promotion found for " + item, Logger.MessageType.Info);
+                     hasDiscount = false;
+                  }
+               }
+               catch (Exception e)
+               {
+                  Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "Error occurred getting promotion for " + fruit[fruitName] + " The error was " + e.Message, Logger.MessageType.Error);
+                  totalPrice = 0.00;
+                  break;
+               }
+
+               try
+               {
+                  // Now work with the fruit - we only need to get prices of fruit that are in the basket, a big reason I used a Dictionary
+                  try
+                  {
+                     if (hasDiscount == true)
+                     {
+                        totalPrice += fruit[fruitName] * Convert.ToDouble(quantity) * discount;
+                     }
+                     else
+                     {
+                        totalPrice += fruit[fruitName] * Convert.ToDouble(quantity);
+                     }
+                  }
+                  catch (KeyNotFoundException ex)
+                  {
+                     Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, fruitName + " called for in the basket does not exist in the list of fruit.  Canceling order...", Logger.MessageType.Error);
+                     totalPrice = 0.00;
+                     break;
+                  }
+               }
+               catch (Exception ex)
+               {
+                  Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "Error occurred getting price for " + fruitName + " The error was " + ex.Message, Logger.MessageType.Error);
+                  totalPrice = 0.00;
+                  break;
+               }
+            }
+         }
+         catch(Exception ex)
+         {
+            Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, ex.Message, Logger.MessageType.Error);
+         }
+
+         return totalPrice;
+      }
+
+      private static Dictionary<string, double> GetFruitAndPrices()
+      {
+         Dictionary<string, double> fruit = new Dictionary<string, double>();
+
+         try
+         {
+            // Read in the promotions data.  Note that I'm using "using" blocks which will clean up closing and freeing resources
+            using (FileStream fs = File.OpenRead("./inputs/Fruit.txt"))
+            {
+               using (StreamReader sr = new StreamReader(fs, System.Text.Encoding.UTF8, true, 128))
+               {
+                  string line;
+                  while ((line = sr.ReadLine()) != null)
+                  {
+                     // Beware of possible empty lines.  Further, each valid line must contain a comma
+                     if (line.Contains(','))
+                     {
+                        string[] lineArray = line.Split(',');
+
+                        // Handle potentially adding a duplicate key.  In this case, we'll continue on and take the one that's already there.
+                        try
+                        {
+                           fruit.Add(lineArray[0].Trim(), Convert.ToDouble(lineArray[1].Trim()));
+                        }
+                        catch (ArgumentException e)
+                        {
+                           Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "A key for " + lineArray[0] + " in the list of fruit already exists.  The duplicate has been ignored and processing will continue.", Logger.MessageType.Warning);
+                        }
+                     }
+                  }
+               }
+            }
+         }
+         catch (Exception e)
+         {
+            Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, e.Message, Logger.MessageType.Error);
+            fruit = null;
+         }
+
+         return fruit;
+      }
+
+      /// <summary>
+      /// This will read the Promotions from a file and put them in a dictionary of the form Fruit, Discount
+      /// </summary>
+      /// <returns>A Dictionary of Fruit,Discount pairs</returns>
+      private static Dictionary<string, double> GetPromotions()
+      {
+         Dictionary<string, double> promotions = new Dictionary<string, double>();
+
+         try
+         {
+            // Read in the promotions data.  Note that I'm using "using" blocks which will clean up closing and freeing resources
+            using (FileStream fs = File.OpenRead("./inputs/Promotions.txt"))
+            {
+               using (StreamReader sr = new StreamReader(fs, System.Text.Encoding.UTF8, true, 128))
+               {
+                  string line;
+                  while ((line = sr.ReadLine()) != null)
+                  {
+                     string[] lineArray = line.Split(',');
+
+                     // Handle potentially adding a duplicate key.  In this case, we'll continue on and take the one that's already there.
+                     try
+                     {
+                        promotions.Add(lineArray[0].Trim(), Convert.ToDouble(lineArray[1].Trim()));
+                     }
+                     catch (ArgumentException e)
+                     {
+                        Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "A key for " + lineArray[0] + " in the promotions list already exists.  The duplicate has been ignored and processing will continue.", Logger.MessageType.Warning);
+                     }
+                  }
+               }
+            }
+         }
+         catch (Exception e)
+         {
+            Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, e.Message, Logger.MessageType.Error);
+            promotions = null;
+         }
+
+         return promotions;
+      }
+
+      /// <summary>
+      /// This will read the Basket from a file and put them in a dictionary of the form Fruit, Quantity
+      /// </summary>
+      /// <returns>A Dictionary of Fruit, Quantity pairs</returns>
+      private static Dictionary<string, int> GetBasket()
+      {
+         Dictionary<string, int> basket = new Dictionary<string, int>();
+
+         try
+         {
+            using (FileStream fs = File.OpenRead("./inputs/Basket.txt"))
+            {
+               using (StreamReader sr = new StreamReader(fs, System.Text.Encoding.UTF8, true, 128))
+               {
+                  string line;
+                  while ((line = sr.ReadLine()) != null)
+                  {
+                     string[] lineArray = line.Split(',');
+
+                     // Handle potentially adding a duplicate key.  In this case, we'll continue on and take the one that's already there.
+                     try
+                     {
+                        basket.Add(lineArray[0].Trim(), Convert.ToInt32(lineArray[1].Trim()));
+                     }
+                     catch (ArgumentException e)
+                     {
+                        Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, "A key for " + lineArray[0] + " in the basket already exists.  The duplicate has been ignored and processing will continue.", Logger.MessageType.Warning);
+                     }
+                  }
+               }
+            }
+         }
+         catch(Exception ex)
+         {
+            Logger.GetLogger().WriteEntry(TJCApplications.FruitCalculator, ex.Message, Logger.MessageType.Error);
+            basket = null;
+         }
+
+         return basket;
+      }
+   }
+}
---
+++ FruitCalculator/README.txt
@@ -0,0 +1,56 @@
+﻿To whom it may concern:
+
+This is my C# solution to the Fruit Calculator coding challenge.  A few things I'd like to address before you run and analyze my code:
+
+1.  This is a good example of how I write code for any production application I work on.  You'll notice that I've include a Logger class (which I wrote years ago) and simply tweaked it
+    to include this exercise.  I find that using it is invaluable as the logs it can produce show me what is going on in the code at critical places.  Since it's not easy for one to
+    debug an app in production, this class outputs a variety of message types (WARNING, MANDATORY, INFO and ERROR) to give a lot of messages one can peruse.  There are many messages that are coming
+    from the logger when you run the application (a console app in this case where the log just writes to said console).  I have a configuration setting in the App.config file called 
+    "loggerVerbosity" that is currently set to true to show all messages.  You can change this setting to false in order to eliminate all INFO messages and just see any ERROR, WARNING or MANDATORY
+    messages.  To me, it's important to have something like this in any production application.  I used my exising logger class because it literally took me just a couple of minutes to incorporate it,
+    but I'm also aware of the Microsoft
+
+2.  The way I approached this challenge, as I do with all coding problems I face, was to carefully read the whole challenge statement a couple of times, walk away, think about what I've read
+    and try to come up with a couple of approaches before I even try to code.  Once I feel I have a really good handle on what's being asked for I'll finally start doing some coding.
+
+3.  In developing a solution for this problem, I found that using C# Dictionaries would lead to an easy, yet efficient one.  Although this exercise is called "Fruit Calculator", I chose to view it
+    in terms of a "Fruit Basket Calculator" which guided my thought of looping the the required fruit in the basket as opposed to looping over all the available fruit and handling the ones that
+    were in the basket.  You could imagine that if you had 3 million different types of fruit possible and only two types of fruit in the basket, it would be much faster to over the basket fruit
+    and just "lookup" their information in the other two dictionaries I used as opposed to the other way around.  This is one thing that helped my solution to be scalable as far as performance is
+    concerned.  The other is that I use simple comma delimited text files to store the input data.  Once can easily add more fruits, promotions and fruit in a basket easily without having to recompile
+    application also helping the application be scalable.  For simplicity, you can bring these files up in the solution, make changes and when the project is run, these files will be copied to the 
+    project output directory from where they are read by the application.
+
+4.  As you'll notice, most of the time I spent writing this application was on error handling.  I've found time spent on error handling is significanlty recouped when the application goes to 
+    production as much time can be saved if there are robust messages in the log if something untoward happens.  It also helps me during development time when I induce a bug or make a mistake.
+    For this little application, I focused a lot on validation of the input data and common mistakes that could occur in when changing data in the files.  Most of this code wouldn't even have to 
+    be touched if one decided to pull data from a database instead of files (of course, you'd have to modify the data access layer, but you wouldn't have to touch the validation code for the
+    data itself).
+
+5.  Finally, you'll notice that I tend to put a lot of comments in my code especially where there is more complicated logic, something tricky is going to happen, or just that I wanted to write a
+    note.  I have found throughout my career that this is a good thing to do especially in the case where you're away from the application for some time (like a few months).  It's nice to have
+    these comments to understand why something was done that particular way.
+
+This was a fun exercise and I enjoyed developing this solution.  I hope you like it!
+
+PROBLEM STATEMENT
+
+Coding challenge
+
+Problem: Fruit calculator
+
+The solution needs to be written in C#. The project type, input and output it’s at the developer’s choice. The candidate should send us the best he/she could do in one hour. We do not want them to spend too much time on this.
+
+We have an application that takes as an input some fruits and their prices, promotions applicable and a basket. The output is the total price.
+
+ Example 1:
+
+Input : Oranges – $10; Apples- $5 ; Promotions: No; Basket: Oranges - 5, Apples 1
+
+Output: Total price= 55 
+
+ Example 2:
+
+Input : Oranges – $10; Apples- $5 ; Promotions: Oranges – 0.5;  Basket: Oranges - 5, Apples 1
+
+Output: Total price= 30 
